a2c7280e7b198d8688a7703d45771c27f8f7c6a1
ldalmeida <ldalmeida.br@gmail.com>
2021-04-14 13:33:14 -0300

    exemplo de um deadlock com semaforo

--- main.py
+++ main.py
@@ -1,45 +1,43 @@
-# -*- coding: ISO-8859-1 -*-
-
-import thread
+import threading
 import time, random
 
-s1 = thread.allocate_lock()
-s2 = thread.allocate_lock()
+s1 = threading._allocate_lock()
+s2 = threading._allocate_lock()
 
 def tempo(i):
    t = random.randint(1,5)
-   print "Processo %i dormindo por %i" %(i, t)
+   print("Processo %i dormindo por %i" %(i, t))
    time.sleep(t)
 
-def thread1():
-   print "Processo 1 - Adquirindo semáforo S1"
+def threading1():
+   print("Processo 1 - Adquirindo semáforo S1")
    s1.acquire()
    time.sleep(1)
-   print "Processo 1 - Adquirindo semáforo S2"
+   print("Processo 1 - Adquirindo semáforo S2")
    s2.acquire()
-   print "Processo 1 - Seção crítica"
+   print ("Processo 1 - Seção crítica")
    tempo(1)
-   print "Processo 1 - Liberando semáforos"
+   print("Processo 1 - Liberando semáforos")
    s1.release()
    s2.release()
-   print "Processo 1 - seção não crítica"
+   print ("Processo 1 - seção não crítica")
    tempo(1)
 
-def thread2():
-   print "Processo 2 - Adquirindo semáforo S2"
+def threading2():
+   print ("Processo 2 - Adquirindo semáforo S2")
    s2.acquire()
    time.sleep(1)
-   print "Processo 2 - Adquirindo semáforo S1"
+   print ("Processo 2 - Adquirindo semáforo S1")
    s1.acquire()
-   print "Processo 2 - Seção crítica"
+   print ("Processo 2 - Seção crítica")
    tempo(2)
-   print "Processo 2 - Liberando semáforos"
+   print ("Processo 2 - Liberando semáforos")
    s2.release()
    s1.release()
-   print "Processo 2 - seção não crítica"
+   print ("Processo 2 - seção não crítica")
    tempo(2)
 
-thread.start_new_thread(thread1, ())
-thread.start_new_thread(thread2, ())
+threading._start_new_thread(threading1, ())
+threading._start_new_thread(threading2, ())
 
 while 1: pass
