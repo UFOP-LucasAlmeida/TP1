4c97bf152ba74c2f00bfe59d1b5e1fa302fd203d
ldalmeida <ldalmeida.br@gmail.com>
2021-04-14 13:41:49 -0300

    Exemplo de um buffer limitado

--- main.py
+++ main.py
@@ -1,43 +1,48 @@
-import threading
 import time, random
+import threading
 
-s1 = threading._allocate_lock()
-s2 = threading._allocate_lock()
+class BufferLimitado:
+   TAM_BUFFER = 5
+   mutex  = threading.Semaphore(1)
+   empty  = threading.Semaphore(TAM_BUFFER)
+   full   = threading.Semaphore(0)
+   buffer = list(range(TAM_BUFFER))
+   cheio  = 0
+   livre  = 0
 
-def tempo(i):
-   t = random.randint(1,5)
-   print("Processo %i dormindo por %i" %(i, t))
-   time.sleep(t)
+   def insert(self, item):
+      self.empty.acquire()
+      self.mutex.acquire()
+      self.buffer[self.livre] = item
+      self.livre = (self.livre + 1) % self.TAM_BUFFER
+      self.mutex.release()
+      self.full.release()
 
-def threading1():
-   print("Processo 1 - Adquirindo semáforo S1")
-   s1.acquire()
-   time.sleep(1)
-   print("Processo 1 - Adquirindo semáforo S2")
-   s2.acquire()
-   print ("Processo 1 - Seção crítica")
-   tempo(1)
-   print("Processo 1 - Liberando semáforos")
-   s1.release()
-   s2.release()
-   print ("Processo 1 - seção não crítica")
-   tempo(1)
+   def remove(self):
+      self.full.acquire()
+      self.mutex.acquire()
+      item = self.buffer[self.cheio]
+      self.cheio = (self.cheio + 1) % self.TAM_BUFFER
+      self.mutex.release()
+      self.empty.release()
+      return item
 
-def threading2():
-   print ("Processo 2 - Adquirindo semáforo S2")
-   s2.acquire()
-   time.sleep(1)
-   print ("Processo 2 - Adquirindo semáforo S1")
-   s1.acquire()
-   print ("Processo 2 - Seção crítica")
-   tempo(2)
-   print ("Processo 2 - Liberando semáforos")
-   s2.release()
-   s1.release()
-   print ("Processo 2 - seção não crítica")
-   tempo(2)
+b = BufferLimitado()
 
-threading._start_new_thread(threading1, ())
-threading._start_new_thread(threading2, ())
+def produtor():
+   while True:
+      time.sleep(random.randint(1, 10) / 100.0)
+      item = time.ctime()
+      b.insert(item)
+      print("Produtor produziu:", item, b.livre, b.cheio)
+
+def consumidor():
+   while True:
+      time.sleep(random.randint(1, 10) / 100.0)
+      item = b.remove()
+      print("Consumidor consumiu:", item, b.livre, b.cheio)
+
+threading._start_new_thread(produtor, ())
+threading._start_new_thread(consumidor, ())
 
 while 1: pass
