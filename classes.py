7e43c960dcca7df111bfcd46b08f177ecd89b633
ldalmeida <ldalmeida.br@gmail.com>
2021-04-14 23:33:33 -0300

    atualização do algoritmo

--- classes.py
+++ classes.py
@@ -7,7 +7,7 @@ class FilaTarefas:
     
     def __init__(self, size):
         self.tamanhoFila = size
-
+          
     def inserir(self,tarefa):
         if len(self.fila) < self.tamanhoFila:
             self.fila.append(tarefa)
@@ -19,15 +19,17 @@ filaTarefas = FilaTarefas(5)
 
 class Funcionario:
     id = 0
-    tarefas = list()
 
     def __init__(self, id):
         self.id = id
+        self.tarefas = []
         f= open("func%i.txt" %(id),"r+")
-        lista = str.split(f.read()," ")
+        lista = f.readlines()
+        f.close()
         for tarefa in lista:
-            self.tarefas.append([id,tarefa])
-
+            tarefa = tarefa.replace('\n', '')
+            self.tarefas.append([id] + tarefa.split(" "))
+                    
 class Maquina:
     id = 0
     idFuncionario = 0
@@ -39,10 +41,12 @@ class Maquina:
         self.id = idM
 
     def executarThread(self):
-        t = threading.Thread(target = self.executarProximaTarefa)
+        t = threading.Thread(target = self.executarTarefas)
         t.start()
 
-    def executarProximaTarefa(self):
-        tarefa = self.filaTarefas.fila[0]
-        time.sleep(tarefa[2])
-        print("Exec", self.id, tarefa[0])
+    def executarTarefas(self):
+        while len(filaTarefas.fila) > 0:
+            tarefa = filaTarefas.fila[0]
+            filaTarefas.remover()
+            time.sleep(tarefa[2])
+            print("Exec", self.id, tarefa[0])
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import classes
 
+listaTarefas = list()
 filaTarefas = classes.FilaTarefas(5)
 listaMaquinas = list()
 listaFuncionarios = list()
@@ -11,23 +12,27 @@ for x in range(numMaquinas):
     listaMaquinas.append(classes.Maquina(idM))
 tamanhoFila = int(input("\n Digite a capacidade da fila de tarefas: "))
 numFuncionarios = int(input("\n Digite o número de funcionários: "))
+
 for x in range(numFuncionarios):
     try:
         f= open("func%i.txt" %(x+1))
+        print("Arquivo do funcionario %i: func%i.txt" %(x+1,x+1))
+        funcionario = classes.Funcionario(x+1)
+        listaFuncionarios.append(funcionario)
     except:
         print("Arquivo do funcionario %i nao encontrado" %(x+1))
     f.close()
 
-    print("Arquivo do funcionario %i: func%i.txt" %(x+1,x+1))
-    listaFuncionarios.append(classes.Funcionario(x+1))
 
 filaTarefas = classes.FilaTarefas(tamanhoFila)
 for maquina in listaMaquinas:
     maquina.filaTarefas = filaTarefas
     
+#for funcionario in listaFuncionarios:
+#    for tarefa in funcionario.tarefas:
+#        listaTarefas.append(tarefa)
 
-filaTarefas.inserir([0,1])
-
-x = 0
+for maquina in listaMaquinas:   
+    maquina.executarThread()
 
 
